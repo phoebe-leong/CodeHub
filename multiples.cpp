1aa3903f87f485d9c757c6123708990a72a3f8a9
PH03be <dariusleong8@gmail.com>
2021-03-11 11:18:04 +1100

    Update multiples.cpp

--- multiples.cpp
+++ multiples.cpp
@@ -3,8 +3,8 @@
  * @file multiples.cpp
  * @author Phoebe Leong (dariusleong8@gmail.com)
  * @brief A simple C++ program which takes user input and then prints out the number and its multiple until the user-inputted number is reached.
- * @version 0.1
- * @date 26-02-21
+ * @version 1.1
+ * @date 11-03-21
  * @copyright Copyright (c) 2021
  */
 
@@ -12,67 +12,16 @@
 #include <unistd.h>
 #include <stdio.h>  
 #include <stdlib.h>
-#include <ctime>
 #include <cmath>
-#include <fstream>
 #include "multiples.h"
 using namespace std;
 
-/*
-
-Devlog:
-
-Friday 26-02-21:
-Made the basis of the program
-
-Saturday 27-02-21:
-Put the definition of multiplication() before multiples() so that it could be run within multiples() instead of in main()
-Did some testing
-Added verifiedInputPlusOne
-Removed verifiedInputPlusOne
-Added randNum
-Did a lot of debugging and edge-case fixing
-Added nanosleep()
-Removed nanosleep()
-
-Sunday 28-02-21:
-Removed #include <time.h>
-Added #include <ctime>
-Added saveFile
-Added save
-Added #include <limits>
-Removed #include <limits>
-Removed saveFile
-Removed save
-
-*/
-
 int input;
 int verifiedInput;
-const int randNum = rand();
+const int num = 50;
 
-void check() {
-    if (input > 1 && input < randNum + 1) {
-        verifiedInput = input;
-
-        multiplication();
-    } else {
-        std::cout << "Your input does not meet the specified requirements. Try again:\n";
-        std::cin >> input;
-
-        check();
-
-    }
-}
-
-void intro() {
-    std::cout << "\n";
-    std::cout << "--Started by Phoebe Leong on 26 / 2 / 2021 at 7:24pm in Sydney, Australia using Visual Studio Code v1.53.2 for MacOS Catalina v10.15.7--\n";
-    std::cout << "--Finished by Phoebe Leong on 28 / 2 / 2021 at 11:13am in Sydney, Australia using Visual Studio Code v1.53.2 for MacOS Catalina v10.15.7--\n";
-    std::cout << "--Originally tested using iTerm v3.4.4 for MacOS Catalina v10.15.7--\n";
-    std::cout << "https://github.com/darius-leong/multiples\n\n";  
-
-    std::cout << "This is a simple C++ program which takes user input and then prints out the number and its multiple until the user-inputted number is reached.\n\n";
+int close () {
+    return 0;
 }
 
 void multiplication() {
@@ -83,8 +32,33 @@ void multiplication() {
     }
 }
 
+void check() {
+    if (input > 1 && input < num + 1) {
+        verifiedInput = input;
+        multiplication();
+    } 
+    if (input < 1 || input > num + 1) {
+        std::cout << "Your input does not meet the specified requirements. Try again:\n";
+        std::cin >> input;
+}
+
+if (input == NAN) {
+    input = 10;
+}
+
+}
+void intro() {
+    std::cout << "\n";
+    std::cout << "--Started by Phoebe Leong on 26 / 2 / 2021 at 7:24pm in Sydney, Australia using Visual Studio Code v1.53.2 for MacOS Catalina v10.15.7--\n";
+    std::cout << "--Finished by Phoebe Leong on 11 / 3 / 2021 at 11:13am in Sydney, Australia using Visual Studio Code v1.54.2 for MacOS Catalina v10.15.7--\n";
+    std::cout << "--Originally tested using iTerm v3.4.4 for MacOS Catalina v10.15.7--\n";
+    std::cout << "https://github.com/phoebe-leong/multiples\n\n";  
+
+    std::cout << "This is a simple C++ program which takes user input and then prints out the number and its multiple until the user-inputted number is reached.\n\n";
+}
+
 void multiples() {
-    std::cout << "Give a number between 1 and " << randNum << ":\n";
+    std::cout << "Give a number between 1 and " << num << ":\n";
     std::cin >> input;
 
     // Checks if the user input meets the 1 - 10 ratio
@@ -93,18 +67,13 @@ void multiples() {
 
 }
 
-int main() {
-
-    srand (time(NULL));
+int main () {
 
     intro();
     multiples();
 
-    std::cout << "\n\n";
-    std::cout << "Press enter to close the program.\n";
-
-    std::cin.get();
-    std::cin.ignore();
-
-    return 0;
+    cout << "Press enter to close the program.\n";
+    cin.ignore();
+    cin.get();
+    close();
 }
