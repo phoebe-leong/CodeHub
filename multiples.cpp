b51f340e89b8e7db31190a59b933710dfbe371d6
PH03be <phoebeleong8@gmail.com>
2021-03-13 16:08:27 +1100

    Update multiples.cpp

--- multiples.cpp
+++ multiples.cpp
@@ -1,10 +1,10 @@
 
 /**
  * @file multiples.cpp
- * @author Phoebe Leong (dariusleong8@gmail.com)
+ * @author PH03be (phoebeleong8@gmail.com)
  * @brief A simple C++ program which takes user input and then prints out the number and its multiple until the user-inputted number is reached.
- * @version 1.1
- * @date 11-03-21
+ * @version 0.2
+ * @date 12-03-21
  * @copyright Copyright (c) 2021
  */
 
@@ -20,9 +20,16 @@ int input;
 int verifiedInput;
 const int num = 50;
 
+// Universal close function to exit the program
+
 int close () {
+    cin.ignore();
+    cin.get();
+
     return 0;
 }
+
+// Operators
 
 void multiplication() {
     for (int i = 0; i < verifiedInput; i++) {
@@ -32,32 +39,49 @@ void multiplication() {
     }
 }
 
+void addition() {
+    for (int i = 0; i < verifiedInput; i++) {
+        int j = i + i;
+        cout << i << " " << j;
+        cout << "\n";
+    }
+}
+
+void operatorCheck() {
+
+    string numUse;
+
+    cout << "Do you want to add, multiply, or divide the numbers?\n";
+    cin >> numUse;
+    cout << "\n\n";
+
+    if (numUse == "add") {
+        addition();
+    } 
+    if (numUse == "multiply") {
+        multiplication();
+    }
+}
+
 void check() {
     if (input > 1 && input < num + 1) {
         verifiedInput = input;
-        multiplication();
+
+        operatorCheck();
     } 
-    if (input < 1 || input > num + 1) {
-        std::cout << "Your input does not meet the specified requirements. Try again:\n";
-        std::cin >> input;
+    if (input < 1 || input > num) {
+        numbers();
+    }
 }
 
-if (input == NAN) {
-    input = 10;
-}
-
-}
 void intro() {
     std::cout << "\n";
-    std::cout << "--Started by Phoebe Leong on 26 / 2 / 2021 at 7:24pm in Sydney, Australia using Visual Studio Code v1.53.2 for MacOS Catalina v10.15.7--\n";
-    std::cout << "--Finished by Phoebe Leong on 11 / 3 / 2021 at 11:13am in Sydney, Australia using Visual Studio Code v1.54.2 for MacOS Catalina v10.15.7--\n";
-    std::cout << "--Originally tested using iTerm v3.4.4 for MacOS Catalina v10.15.7--\n";
     std::cout << "https://github.com/phoebe-leong/multiples\n\n";  
 
     std::cout << "This is a simple C++ program which takes user input and then prints out the number and its multiple until the user-inputted number is reached.\n\n";
 }
 
-void multiples() {
+void numbers() {
     std::cout << "Give a number between 1 and " << num << ":\n";
     std::cin >> input;
 
@@ -70,10 +94,8 @@ void multiples() {
 int main () {
 
     intro();
-    multiples();
+    numbers();
 
     cout << "Press enter to close the program.\n";
-    cin.ignore();
-    cin.get();
     close();
 }
