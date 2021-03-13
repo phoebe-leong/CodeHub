df594b7fe8a6d9c1c4af7f608b3414c7f85b167b
PH03be <phoebeleong8@gmail.com>
2021-03-14 09:16:06 +1100

    Update multiples.cpp

--- multiples.cpp
+++ multiples.cpp
@@ -9,6 +9,7 @@
  */
 
 #include <iostream>
+#include <fstream>
 #include <unistd.h>
 #include <stdio.h>  
 #include <stdlib.h>
@@ -18,11 +19,15 @@ using namespace std;
 
 int input;
 int verifiedInput;
+ofstream file;
 const int num = 50;
+
 
 // Universal close function to exit the program
 
 int close () {
+    cout << "Press enter to close the program.\n";
+
     cin.ignore();
     cin.get();
 
@@ -32,70 +37,96 @@ int close () {
 // Operators
 
 void multiplication() {
+    file.open("output.txt");
+
     for (int i = 0; i < verifiedInput; i++) {
         int j = i * i;
-        std::cout << i << " " << j;
-        std::cout << "\n";
+        cout << i << " " << j;
+        file << i << " " << j;
+        cout << "\n";
+        file << "\n";
     }
+
+    file.close();
 }
 
 void addition() {
+    file.open("output.txt");
+
     for (int i = 0; i < verifiedInput; i++) {
         int j = i + i;
         cout << i << " " << j;
+        file << i << " " << j;
         cout << "\n";
+        file << "\n";
     }
+
+    file.close();
+}
+
+void subtraction() {
+    file.open("output.txt");
+
+    for (int i = 0; i < verifiedInput; i++) {
+        int j = i;
+        j -= 1;
+        cout << i << " " << j;
+        file << i << " " << j;
+        cout << "\n";
+        file << "\n";
+    }
+
+    file.close();
 }
 
 void operatorCheck() {
-
     string numUse;
+    int numUseInt;
 
-    cout << "Do you want to add, multiply, or divide the numbers?\n";
+    cout << "Do you want to add, multiply, or subtract the numbers?\n";
     cin >> numUse;
     cout << "\n\n";
 
     if (numUse == "add") {
         addition();
-    } 
-    if (numUse == "multiply") {
+    } else if (numUse == "multiply") {
         multiplication();
+    } else if (numUse == "subtract") {
+        subtraction();
     }
 }
 
 void check() {
+
     if (input > 1 && input < num + 1) {
         verifiedInput = input;
-
         operatorCheck();
-    } 
-    if (input < 1 || input > num) {
+
+    } else if (input < 1 || input > num) {
         numbers();
     }
 }
 
 void intro() {
-    std::cout << "\n";
-    std::cout << "https://github.com/phoebe-leong/multiples\n\n";  
+    cout << "\n";
+    cout << "https://github.com/phoebe-leong/multiples\n\n";  
 
     std::cout << "This is a simple C++ program which takes user input and then prints out the number and its multiple until the user-inputted number is reached.\n\n";
 }
 
 void numbers() {
-    std::cout << "Give a number between 1 and " << num << ":\n";
-    std::cin >> input;
+
+    cout << "Give a number between 1 and " << num << ":\n";
+    cin >> input;
 
     // Checks if the user input meets the 1 - 10 ratio
 
     check();
-
 }
 
 int main () {
-
     intro();
     numbers();
-
-    cout << "Press enter to close the program.\n";
     close();
+
 }
