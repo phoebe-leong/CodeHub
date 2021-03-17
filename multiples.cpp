e9efa3394b6d4d68fa4f097374bf018ad90b4430
PH03be <phoebeleong8@gmail.com>
2021-03-17 17:39:15 +1100

    Update multiples.cpp

--- multiples.cpp
+++ multiples.cpp
@@ -3,21 +3,22 @@
  * @file multiples.cpp
  * @author PH03be (phoebeleong8@gmail.com)
  * @brief A simple C++ program which takes user input and then prints out the number and its multiple until the user-inputted number is reached.
- * @version 0.3
+ * @version 1.3
  * @date 12-03-21
  * @copyright Copyright (c) 2021
  */
 
 #include <iostream>
 #include <string>
+#include <math.h>
 #include "multiples.h"
 using namespace std;
 
-int input;
-int verifiedInput;
+int input;  
+double verifiedInput;
 const int num = 50;
 
-// Universal close function to exit the program
+// Universal close function/s to exit the program
 
 int close () {
     cout << "Press enter to close the program.\n";
@@ -28,10 +29,14 @@ int close () {
     return 0;
 }
 
+int forceClose () {
+    return 0;
+}
+
 // Operators
 
 void multiplication() {
-    for (int i = 0; i < verifiedInput; i++) {
+    for (int i = 0; i <= verifiedInput; i++) {
         int j = i * i;
         cout << i << " " << j;
         cout << "\n";
@@ -39,7 +44,7 @@ void multiplication() {
 }
 
 void addition() {
-    for (int i = 0; i < verifiedInput; i++) {
+    for (int i = 1; i <= verifiedInput; i++) {
         int j = i + i;
         cout << i << " " << j;
         cout << "\n";
@@ -47,19 +52,28 @@ void addition() {
 }
 
 void subtraction() {
-    for (int i = 0; i < verifiedInput; i++) {
-        int j = i;
-        j -= 1;
+    for (int i = 1; i <= verifiedInput; i++) {
+        int j = i - 1;
         cout << i << " " << j;
         cout << "\n";
     }
 }
 
+void division() {
+    for (int i = 1; i <= verifiedInput; i++) {
+        int j = i % i;
+        cout << i << " " << j;
+        cout << "\n";
+    }
+}
+
+// Operators - end
+
 void operatorCheck() {
     string numUse;
     int numUseInt;
 
-    cout << "Do you want to add, multiply, or subtract the numbers?\n";
+    cout << "Do you want to add, multiply, divide, or subtract the numbers?\n";
     cin >> numUse;
     cout << "\n\n";
 
@@ -69,6 +83,8 @@ void operatorCheck() {
         multiplication();
     } else if (numUse == "subtract") {
         subtraction();
+    } else if (numUse == "divide") {
+        division();
     }
 }
 
@@ -77,20 +93,22 @@ void check() {
     if (input > 1 && input < num + 1) {
         verifiedInput = input;
         operatorCheck();
-
     } else if (input < 1 || input > num) {
-        numbers();
+        numberInput();
+    } else {
+        // doesnt work - why not?
+        forceClose();
     }
 }
 
 void intro() {
     cout << "\n";
-    cout << "https://github.com/phoebe-leong/multiples\n\n";  
+    cout << "https://github.com/phoebe-leong/multiples" << "\n\n";  
 
-    cout << "This is a simple C++ program which takes user input and then prints out the number and its multiple until the user-inputted number is reached.\n\n";
+    cout << "This is a simple C++ program which takes user input and then prints out the number and the answer to the selected equation until the user-inputted number is reached.\n\n";
 }
 
-void numbers() {
+void numberInput() {
 
     cout << "Give a number between 1 and " << num << ":\n";
     cin >> input;
@@ -102,7 +120,7 @@ void numbers() {
 
 int main () {
     intro();
-    numbers();
+    numberInput();
     close();
 
 }
