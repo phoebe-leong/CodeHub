06723d18a25ec511498f4f570d19f8b5932c8688
PH03be <phoebeleong8@gmail.com>
2021-05-07 14:04:53 +1000

    fixed crash when NaN character entered
    also fixed edgecase related to crash fix

--- main.cpp
+++ main.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
 
-// variable declarations
-
-int input;
-
 int main() {
+    std::string input;
+
+    system("clear");
+
     std::cout << "================================================\n";
     std::cout << "              Made by PH03be\n";
     std::cout << "        [ 0 ] for multiplication, etc\n";
     std::cout << "        [ 1 ] for text analysis\n";
     std::cout << "        [ 2 ] for 'Capitalism In A Nutshell'\n";
+    std::cout << "        [ 3 ] for [A-N-I-M-E u-w-u]\n";
+    std::cout << "        [ 4 ] for rock, paper, scissors\n";
     std::cout << "More will hopefully come in the near-ish future\n";
     std::cout << "================================================\n";
     std::cin >> input;
 
-    if (input == 0) {
+    if (input == "0") {
         system("g++ -o multiples.out math.cpp");
         system("clear");
         system("./multiples.out");
-    } else if (input == 1) {
+    } else if (input == "1") {
         system("g++ -o string.out string.cpp");
         system("clear");
         system("./string.out");
-    } else if (input == 2) {
+    } else if (input == "2") {
         system("clear");
         system("./main");
+    } else if (input == "3") {
+        system("clear");
+        system("./anime");
+    } else if (input == "4") {
+        system("clear");
+        system("./rock-paper-scissors");
+    } else {
+        system("clear");
+        std::cout << "Incorrect character. Select one of the options from the main menu.\n";
+        std::cin.ignore();
+        std::cin.get();
+
+        main();
     }
 }
