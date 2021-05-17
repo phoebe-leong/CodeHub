6321f0f60f8cada0210c95814ec1db6d10725aa8
PH03be <phoebeleong8@gmail.com>
2021-05-17 18:03:48 +1000

    made the program compile everything before run

--- main.cpp
+++ main.cpp
@@ -7,35 +7,41 @@ int main() {
 
     std::cout << "================================================\n";
     std::cout << "              Made by PH03be\n";
-    std::cout << "        [ 0 ] for multiplication, etc\n";
-    std::cout << "        [ 1 ] for text analysis\n";
-    std::cout << "        [ 2 ] for 'Capitalism In A Nutshell'\n";
-    std::cout << "        [ 3 ] for [A-N-I-M-E u-w-u]\n";
-    std::cout << "        [ 4 ] for rock, paper, scissors\n";
-    std::cout << "More will hopefully come in the near-ish future\n";
+    std::cout << "        [ 1 ] for multiplication, etc\n";
+    std::cout << "        [ 2 ] for text analysis\n";
+    std::cout << "        [ 3 ] for 'Capitalism In A Nutshell'\n";
+    std::cout << "        [ 4 ] for a random anime character generator\n";
+    std::cout << "        [ 5 ] for rock, paper, scissors\n";
+    std::cout << "        [ 6 ] to exit the program\n";
     std::cout << "================================================\n";
     std::cin >> input;
 
-    if (input == "0") {
+    if (input == "1") {
         system("g++ -o multiples.out math.cpp");
         system("clear");
         system("./multiples.out");
-    } else if (input == "1") {
+    } else if (input == "2") {
         system("g++ -o string.out string.cpp");
         system("clear");
         system("./string.out");
-    } else if (input == "2") {
-        system("clear");
-        system("./main");
     } else if (input == "3") {
+        system("g++ -o CIAN.out CIAN.cpp");
         system("clear");
-        system("./anime");
+        system("./CIAN.out");
     } else if (input == "4") {
+        system("g++ -o anime.out anime.cpp");
         system("clear");
-        system("./rock-paper-scissors");
+        system("./anime.out");
+    } else if (input == "5") {
+        system("g++ -o rockpaperscissors.out rockpaperscissors.cpp");
+        system("clear");
+        system("./rockpaperscissors.out");
+    } else if (input == "6") {
+        system("clear");
+        return 0;
     } else {
         system("clear");
-        std::cout << "Incorrect character. Select one of the options from the main menu.\n";
+        std::cout << "Not an input. Select one of the options from the main menu.\n";
         std::cin.ignore();
         std::cin.get();
 
