521c1931b44b06a45278182ac7c1fcfb12057176
PH03be <dariusleong8@gmail.com>
2021-05-23 12:49:00 +1000

    added shell files

---
+++ exit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -f string.out ]
+then
+    rm string.out 
+fi
+
+if [ -f math.out ]
+then
+    rm math.out
+fi
+
+if [ -f menu.out ]
+then
+    rm menu.out
+fi
+
+if [ -f a.out ]
+then
+    rm a.out
+fi
+
+if [ -f CIAN.out ]
+then
+    rm CIAN.out
+fi
+
+if [ -f rockpaperscissors.out ]
+then
+    rm rockpaperscissors.out
+fi
+
+exit 0;
