4893ba366b374c93c910bc84a0f38dc91817e2b4
PH03be <phoebeleong8@gmail.com>
2021-05-26 17:14:53 +1000

    removed CIAN

--- main.cpp
+++ main.cpp
@@ -9,10 +9,9 @@ int main() {
     std::cout << "                Made by PH03be\n";
     std::cout << "        [ 1 ] for multiplication, etc\n";
     std::cout << "        [ 2 ] for text analysis\n";
-    std::cout << "        [ 3 ] for 'Capitalism In A Nutshell'\n";
-    std::cout << "        [ 4 ] for a random anime character generator\n";
-    std::cout << "        [ 5 ] for rock, paper, scissors\n";
-    std::cout << "        [ 6 ] to exit the program\n";
+    std::cout << "        [ 3 ] for a random anime character generator\n";
+    std::cout << "        [ 4 ] for rock, paper, scissors\n";
+    std::cout << "        [ 5 ] to exit the program\n";
     std::cout << "================================================\n";
     std::cin >> input;
 
@@ -25,18 +24,14 @@ int main() {
         system("clear");
         system("./string.out");
     } else if (input == "3") {
-        system("g++ -o CIAN.out CIAN.cpp");
-        system("clear");
-        system("./CIAN.out");
-    } else if (input == "4") {
         system("g++ -o anime.out anime.cpp");
         system("clear");
         system("./anime.out");
-    } else if (input == "5") {
+    } else if (input == "4") {
         system("g++ -o rockpaperscissors.out rockpaperscissors.cpp");
         system("clear");
         system("./rockpaperscissors.out");
-    } else if (input == "6") {
+    } else if (input == "5") {
         system("clear");
         system("chmod +x exit.sh");
         system("./exit.sh");
