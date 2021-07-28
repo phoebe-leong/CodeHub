4cf8d659999dbc9829129e5b17712c050c276971
ph3b3 <phoebeleong8@gmail.com>
2021-07-28 11:00:16 +1000

    delete all .out files

--- run.sh
+++ run.sh
@@ -1,36 +1,5 @@
 #!/bin/bash
 
-# Checks for any of the .out files left behind from the last program and deletes them
-
-if [ -f string.out ]
-then
-    rm string.out 
-fi
-
-if [ -f math.out ]
-then
-    rm math.out
-fi
-
-if [ -f menu.out ]
-then
-    rm menu.out
-fi
-
-if [ -f a.out ]
-then
-    rm a.out
-fi
-
-if [ -f rockpaperscissors.out ]
-then
-    rm rockpaperscissors.out
-fi
-
-if [ -f anime.out ]
-then
-    rm anime.out
-fi
-
+rm *.out
 g++ -o menu.out main.cpp
 ./menu.out
